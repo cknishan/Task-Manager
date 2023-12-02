57cf0e85329dc331f06f4e8841f70f4591689776
Nishan Chakma <nishan.chakma.nish@gmail.com>
2023-12-02 17:08:21 +1100

    react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import CreateTask from './pages/CreateTask'
+import ShowTask from './pages/ShowTask'
+import DeleteTask from './pages/DeleteTask'
+import EditTask from './pages/EditTask'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/tasks/create' element={<CreateTask />} />
+      <Route path='/tasks/details/:id' element={<ShowTask />} />
+      <Route path='/tasks/edit/:id' element={<EditTask />} />
+      <Route path='/tasks/delete/:id' element={<DeleteTask />} />
+    </Routes>
   )
 }
---
+++ frontend/src/pages/DeleteTask.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DeleteTask = () => {
+    return (
+        <div>DeleteTask</div>
+    )
+}
